26603a2897491634c6dd71e8a1cc8d37fb3c5258
penvarict <cpenvari@uccs.edu>
2022-04-26 00:12:36 +0000

    add bs5 support and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/projects/new', to: "projects#new"
   get '/projects/edit', to: "projects#edit"
   get '/projects/:id', to: "projects#show" 
+  get '/students/sign_in(.:format)', to: "devise/sessions#new"
+  
+
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
